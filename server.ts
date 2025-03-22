16e94492a24913550b14f0fcffdd0cb9662ac427
Jakov C <qwerasdftzu@gmail.com>
2025-03-22 14:58:12 +0100

    Set samesite to none temporarily

--- server.ts
+++ server.ts
@@ -33,7 +33,7 @@ app.use(
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             maxAge: 1000 * 60 * 60 * 24, // 24 hours
-            sameSite: "lax",
+            sameSite: "none",
         },
         // For production, use a proper store like:
         store: MongoStore.create({ mongoUrl: getEnv().MONGO_URI }),
